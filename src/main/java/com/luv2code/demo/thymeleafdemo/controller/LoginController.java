b8a067d5881cdbc977128c3110b33e4efd750fe6
unknown <abhhishek003@gmail.com>
2022-03-24 22:51:45 +0530

    Added login/logout functionality with in-memory authentication

---
+++ src/main/java/com/luv2code/demo/thymeleafdemo/controller/LoginController.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.luv2code.demo.thymeleafdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @author Abhishek
+ *
+ */
+@Controller
+public class LoginController {
+
+	@GetMapping("/showMyLoginPage")
+	public String showMyLoginPage() {
+		return "login";
+	}
+}
