d786e151b0f11b257c343b24e7dcf954568ca581
unknown <abhhishek003@gmail.com>
2022-03-27 19:46:55 +0530

    Added jdbc authentication

--- src/main/java/com/luv2code/demo/thymeleafdemo/security/SecurityConfig.java
+++ src/main/java/com/luv2code/demo/thymeleafdemo/security/SecurityConfig.java
@@ -3,13 +3,15 @@
  */
 package com.luv2code.demo.thymeleafdemo.security;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.User.UserBuilder;
 
 /**
  * @author Abhishek
@@ -19,17 +21,23 @@ import org.springframework.security.core.userdetails.User.UserBuilder;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
+	@Autowired
+	@Qualifier("securityDataSource")
+	private DataSource securityDataSource;
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		
-		//add users for in memory authenication
+		auth.jdbcAuthentication().dataSource(securityDataSource);
 		
-		UserBuilder users = User.withDefaultPasswordEncoder();
-		
-		auth.inMemoryAuthentication()
-		.withUser(users.username("john").password("test123").roles("EMPLOYEE"))
-		.withUser(users.username("mary").password("test123").roles("MANAGER"))
-		.withUser(users.username("susan").password("test123").roles("ADMIN"));
+//		//add users for in memory authenication
+//		
+//		UserBuilder users = User.withDefaultPasswordEncoder();
+//		
+//		auth.inMemoryAuthentication()
+//		.withUser(users.username("john").password("test123").roles("EMPLOYEE"))
+//		.withUser(users.username("mary").password("test123").roles("MANAGER"))
+//		.withUser(users.username("susan").password("test123").roles("ADMIN"));
 		
 	}
 
